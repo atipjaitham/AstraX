4a5adda9f6565ced338199ebca1656470ff889dd
Atip Jaitham <atip@Atips-MacBook-Pro.local>
2026-06-24 00:13:03 +0700

    feat: initialize AstraX backend foundation and prisma setup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,8 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ProfilesModule } from './profiles/profiles.module';
+
 @Module({
-  imports: [],
+  imports: [
+    PrismaModule,
+    AuthModule,
+    UsersModule,
+    ProfilesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
